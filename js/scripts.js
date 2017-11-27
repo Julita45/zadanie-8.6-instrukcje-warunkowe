0b910cfac4aaf5bdb23e0f585edf7b5e3631ef58
Julita Lutostańska <julita.lutostanska@gmail.com>
2017-11-27 21:04:47 +0100

    Change index.html and add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var a = 2,
+	b = 3;
+var value; 
+
+a = prompt ('enter a', a);
+b = prompt ('enter b', b);
+
+value = (a * a) - (2 * a * b) + (b * b);
+
+if (value > 0) {
+	alert('Value is positive');
+	console.log('Value is positive');
+} else if (value < 0) {
+	alert('Value is negative');
+	console.log('Value is negative');
+} else {
+	alert('Value is zero');
+	console.log('Value is zero');
+}
